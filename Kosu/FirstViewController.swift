8729b532f4add194364d57986acb04eda7160b38
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-14 10:32:07 +0300

    Initial Commit

---
+++ Kosu/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kosu
+//
+//  Created by Furkan Sabaz on 14.06.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
